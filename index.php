d80d16a1b45fc5798c2f5e943145a43864df9e61
Ismael Reyes <ismael.kix2902@gmail.com>
2018-03-08 19:21:42 +0100

    Updated indentation of html form

--- index.php
+++ index.php
@@ -5,12 +5,12 @@
 </head>
 <body>
 
-<!-- Input data form -->
-<form action="" method="post">
-<label for="url">Url:</label>
-<input type="text" name="url" id="url" />
-<button type="submit">Count shares</button>
-</form>
+    <!-- Input data form -->
+    <form action="" method="post">
+        <label for="url">Url:</label>
+        <input type="text" name="url" id="url" />
+        <button type="submit">Count shares</button>
+    </form>
 
 
 
