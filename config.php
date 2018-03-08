8392d15671450158998d93886d95d4b6f6624c05
Ismael Reyes <ismael.kix2902@gmail.com>
2018-03-08 20:16:54 +0100

    Added PDO needed data in config file

--- config.php
+++ config.php
@@ -1,10 +1,18 @@
 <?php
 
 // Database configuration
-$host = 'localhost';
+$host = "localhost";
 $port = 8889;
-$user = 'root';
-$password = 'root';
-$db = 'share_count';
+$user = "root";
+$password = "root";
+$db = "share_count";
+$charset = "utf8mb4";
+
+$dsn = "mysql:host=$host;dbname=$dbname;port=$port;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
 
 ?>
