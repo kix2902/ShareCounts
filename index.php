1ff3fb2fe19e541872214e5e601765537d8d4a13
Ismael Reyes <ismael.kix2902@gmail.com>
2018-03-08 19:20:45 +0100

    Create simple html structure for index file

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Share counter</title>
+</head>
+<body>
+
+<!-- Input data form -->
+<form action="" method="post">
+<label for="url">Url:</label>
+<input type="text" name="url" id="url" />
+<button type="submit">Count shares</button>
+</form>
+
+
+
+
+</body>
+</html>
