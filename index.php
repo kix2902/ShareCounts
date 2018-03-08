7afbb89709db5f937cc86e994ed97782c26705f1
Ismael Reyes <ismael.kix2902@gmail.com>
2018-03-08 20:17:29 +0100

    Updated index file with check of sent data and creation of PDO object

--- index.php
+++ index.php
@@ -5,14 +5,21 @@
 </head>
 <body>
 
-    <!-- Input data form -->
     <form action="" method="post">
         <label for="url">Url:</label>
         <input type="text" name="url" id="url" />
         <button type="submit">Count shares</button>
     </form>
 
+<?php
+include 'config.php';
 
+if (isset($_POST['url'])) {
+   $connection = new PDO($dsn, $user, $password, $options); 
+
+}
+
+?>
 
 
 </body>
