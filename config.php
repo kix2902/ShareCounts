c8b607b30b5c8622d158b0cca61b45895ef4d12b
Ismael Reyes <ismael.kix2902@gmail.com>
2018-03-10 03:21:27 +0100

    Fixed bug with PDO dsn string declaration

--- config.php
+++ config.php
@@ -8,7 +8,7 @@ $password = "root";
 $db = "share_count";
 $charset = "utf8mb4";
 
-$dsn = "mysql:host=$host;dbname=$dbname;port=$port;charset=$charset";
+$dsn = "mysql:host=$host;dbname=$db;port=$port;charset=$charset";
 $options = [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
