49696a92ec9b603ba5a7fea8dc01a3c7c16c4b13
Ismael Reyes <ismael.kix2902@gmail.com>
2018-03-10 03:21:43 +0100

    Added Twitter tokens to config file

--- config.php
+++ config.php
@@ -1,5 +1,17 @@
 <?php
 
+// Twitter data
+$twitter_api_key = "cpQJ8EBiKLXT0ZklrgfOBDSf0";
+$twitter_api_secret = "YH5L2XjznjrOfwpb4HvzVUvnOZUd381jBUPmP0Let9i0XLa00A";
+$twitter_access_token = "17373305-3Nsg9zKnU9Yv8EFSNqLFTTtlINuu8ktCkjtMFVLnC";
+$twitter_access_token_secret = "iFpsvW4oWVa8gtuSeFhfxzDn3gXib8DFfUOT2ZDhtenFP";
+$twitter_settings = array(
+    'oauth_access_token' => $twitter_access_token,
+    'oauth_access_token_secret' => $twitter_access_token_secret,
+    'consumer_key' => $twitter_api_key,
+    'consumer_secret' => $twitter_api_secret
+);
+
 // Database configuration
 $host = "localhost";
 $port = 8889;
