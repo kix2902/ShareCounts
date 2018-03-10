31403d70d77e7396fd5495957046a544c21fdd02
Ismael Reyes <ismael.kix2902@gmail.com>
2018-03-10 03:22:04 +0100

    Created utils file with a method to decode jsonp responses

---
+++ utils.php
@@ -0,0 +1,9 @@
+<?php
+// Helper method to decode jsonp responses
+function jsonp_decode($jsonp, $assoc = false) { // PHP 5.3 adds depth as third parameter to json_decode
+    if($jsonp[0] !== '[' && $jsonp[0] !== '{') { // we have JSONP
+       $jsonp = substr($jsonp, strpos($jsonp, '('));
+    }
+    return json_decode(trim($jsonp,'();'), $assoc);
+}
+?>
